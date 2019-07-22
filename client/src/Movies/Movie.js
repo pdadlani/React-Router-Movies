66df7395e335e7f8ee2407b790e2211ffaa75178
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2019-07-22 16:23:42 -0700

    refactored code in Movie & MovieDetails to use MovieCard.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard.js';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
   // changed above line, useState({}) to useState() to make it work.*********
   // console.log('props in Movie.js:', props);
-  // const id = props.match.params.id;
+  const id = props.match.params.id;
 
  
   useEffect(() => {
-    const id = props.match.params.id;
+    // const id = props.match.params.id;
+    // moved const id (line above) outside of useEffect, so it can be accessed as first input argument. otherwise, it is unavailable outside the scope of useEffect().
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -22,7 +24,7 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
+  },[id]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
@@ -34,25 +36,10 @@ const Movie = (props) => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  // const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      <MovieCard movie={movie} />
       <div className="save-button">Save</div>
     </div>
   );
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard.js';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -29,27 +30,12 @@ const MovieList = props => {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
+  // const { title, director, metascore, stars } = movie;
   console.log('movie in MovieDetails', movie);
   return (
-    <div className="movie-card">
-      <Link to= {`/movies/${movie.id}`} >
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </Link>
-    </div>
+    <Link to={`/movies/${movie.id}`} >
+      <MovieCard movie={movie} />
+    </Link>
   );
 }
 
